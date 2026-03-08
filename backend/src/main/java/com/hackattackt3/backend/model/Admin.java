586dd1d29765a873a9c6944acd299ba0ba7dcc1f
Camilla Cloutier <camillacloutier45@gmail.com>
2026-03-07 21:24:05 -0800

    wrote missing getters and setters

--- backend/src/main/java/com/hackattackt3/backend/model/Admin.java
+++ backend/src/main/java/com/hackattackt3/backend/model/Admin.java
@@ -3,4 +3,14 @@ package com.hackattackt3.backend.model;
 public class Admin extends User {
     private String firstName;
     private String lastName;
+    private String email;
+
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
 }
